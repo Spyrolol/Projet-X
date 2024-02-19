c2fb2f3c62efca9c9d99c84e067a095405961398
Spyrolol <149724646+Spyrolol@users.noreply.github.com>
2024-02-18 22:07:43 -0500

    v_f

---
+++ svelte_project/src/guides/index.svelte
@@ -0,0 +1,7 @@
+<div class="guides">
+  <ul>
+    <li><a href="/">guide 1</a></li>
+
+    <li><a href="/">guide 2</a></li>
+  </ul>
+</div>
---
+++ svelte_project/src/routes/app.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  import { navigate } from 'svelte-routing';
+  const handleClick = () => {
+  navigate('/settings');
+};
+  const name = " Benzo";
+  let coffeType = "Hot";
+</script>
+
+<div class="index">
+  <h1>Hey,{name}</h1>
+  <p>Votre type de cafe:{coffeType}</p>
+</div>
+<button on:click={handleClick}>Go to Result Page</button>
+<style>
+  h1 {
+    color: white;
+  }
+</style>
